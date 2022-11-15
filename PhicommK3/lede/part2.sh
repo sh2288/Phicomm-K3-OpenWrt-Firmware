f409bb55b2eff0c7b5b6e4e0ad7bd95eed03d543
sh2288 <30304404+sh2288@users.noreply.github.com>
2022-11-15 15:02:11 +0800

    Update part2.sh

--- PhicommK3/lede/part2.sh
+++ PhicommK3/lede/part2.sh
@@ -17,7 +17,7 @@
 #
 
 # Alter default router IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.100.2/g' package/base-files/files/bin/config_generate
 
 #修改NTP设置
 #sed -i "s/'0.openwrt.pool.ntp.org'/'ntp1.aliyun.com'/g" package/base-files/files/bin/config_generate
@@ -32,13 +32,13 @@ sed -i "s/hostname='OpenWrt'/hostname='Phicomm-K3'/g" package/base-files/files/b
 cat package/base-files/files/bin/config_generate |grep hostname=
 echo '=========Alert hostname OK!========='
 
-echo '移除主页跑分信息显示'
-sed -i 's/ <%=luci.sys.exec("cat \/etc\/bench.log") or ""%>//g' package/lean/autocore/files/arm/index.htm
-echo '=========Remove benchmark display in index OK!========='
+#echo '移除主页跑分信息显示'
+#sed -i 's/ <%=luci.sys.exec("cat \/etc\/bench.log") or ""%>//g' package/lean/autocore/files/arm/index.htm
+#echo '=========Remove benchmark display in index OK!========='
 
-echo '移除主页日志打印'
-sed -i '/console.log(mainNodeName);/d' package/lean/luci-theme-argon/htdocs/luci-static/argon/js/script.js
-echo '=========Remove log print in index OK!========='
+#echo '移除主页日志打印'
+#sed -i '/console.log(mainNodeName);/d' package/lean/luci-theme-argon/htdocs/luci-static/argon/js/script.js
+#echo '=========Remove log print in index OK!========='
 
 echo '修改upnp绑定文件位置'
 sed -i 's/\/var\/upnp.leases/\/tmp\/upnp.leases/g' feeds/packages/net/miniupnpd/files/upnpd.config
